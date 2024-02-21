bc351c604bcc3e3737c1f3b60b44a8cce6de88e7
GETASOHARD <Thammarat233@gmail.com>
2024-02-21 15:57:14 +0700

    update 1

--- bor-vor-chor/src/App.js
+++ bor-vor-chor/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import profile from './images/profile_01.jpg'; 
+import half from './images/half.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='header_01'>
+        <div className='sub_header_01'>
+            <img className='profile_01' src={profile} alt='thename'/>
+            <h1>วัน dddd ที่ dd เดือน MMMM ปี yyyy</h1>
+            <h2>ขึ้น/ แรม XX ค่ำ เดือน MM</h2>
+            <div className='scheduled'>
+              <div className='time'>
+                <span>xx:xx</span> <br />
+                <span>xx:xx</span> <br />
+                <span>xx:xx</span> <br />
+                <span>xx:xx</span> <br />
+              </div>
+              <div className='half'>
+                <img src={half} alt='dot' />
+              </div>
+              <div className='event '>
+                  <span className='text-blue-400' >-- none --</span> <br />
+                  <span>-- none --</span> <br />
+                  <span>-- none --</span> <br />
+                  <span>-- none --</span> <br />
+              </div>
+            </div>
+        </div>
+        <div className='sub_header_02'>
+
+        </div>
+      </div>
     </div>
   );
 }
